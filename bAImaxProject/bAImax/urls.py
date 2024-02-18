c223d618cfb34f31434b181c73cdf56e291130b6
Aquinter10 <ndroxgoodplayer@gmail.com>
2024-02-17 23:17:04 -0500

    Home page update.

--- bAImaxProject/bAImax/urls.py
+++ bAImaxProject/bAImax/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from chatbot import views as chatviews
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('chatbot.urls'))
+    path('',chatviews.home)
 ]
--- bAImaxProject/chatbot/views.py
+++ bAImaxProject/chatbot/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request,'home.html')
