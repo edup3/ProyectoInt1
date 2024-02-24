ef87fdedae72ea1bfbaabc7bbf7f01cf993e5315
Natalia <nataliaceballospos@gmail.com>
2024-02-24 13:57:30 -0500

    Models Added

--- bAImaxProject/chatbot/models.py
+++ bAImaxProject/chatbot/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+  name = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  adress = models.CharField(max_length=255)
+  phone = models.IntegerField()
+
+class MedicalAppointment(models.Model):
+  id_specialist = models.IntegerField()
+  id_user = models.IntegerField()
+  confirmation = models.CharField(max_length=255)
+
+class Symptoms(models.Model):
+  description = models.TextField()
+
+class Diagnosis(models.Model):
+  diagnosis = models.TextField()
+  recomendations = models.TextField()
+
+class Specialist(models.Model):
+  name = models.CharField(max_length=255)
+  specialty = models.CharField(max_length=255)
+  phone = models.IntegerField()
+  location = models.CharField(max_length=255)
+  schedule = models.CharField(max_length=255)
+  id_medicalCenter = models.CharField(max_length=255)
+
+class MedicalCenter(models.Model):
+  name = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  adress = models.CharField(max_length=255)
+  phone = models.IntegerField()
+  schedule = models.CharField(max_length=255)
