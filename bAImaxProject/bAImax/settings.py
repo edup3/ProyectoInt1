4b09c7e452f916d2aaecf7a348c18929ea79b53f
Natalia <nataliaceballospos@gmail.com>
2024-05-04 17:43:15 -0500

    Allowed Hosts Modified

--- bAImaxProject/bAImax/settings.py
+++ bAImaxProject/bAImax/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qkt=&%53m&8&6cw!^o!lk2r)3k$d^6nsqb)asgobr2z67&jlyl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['35.202.23.241','*']
 
 
 # Application definition
